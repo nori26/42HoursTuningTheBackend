ea294e00c36b19f018425ae591d4599c9d7349e5
nori <nori623p@gmail.com>
2022-04-15 23:48:28 +0200

    a

--- development/backend/src/api.js
+++ development/backend/src/api.js
@@ -307,7 +307,7 @@ const tomeActive = async (req, res) => {
   const searchGroupQs = 'select * from group_info where group_id = ?';
   const searchThumbQs =
     'select * from record_item_file where linked_record_id = ? order by item_id asc limit 1';
-  const countQs = 'select count(*) from record_comment where linked_record_id = ? limit 1';
+  const countQs = 'select count(*) from record_comment where linked_record_id = ?';
   const searchLastQs = 'select * from record_last_access where user_id = ? and record_id = ?';
   
   puttime(start, i++);
