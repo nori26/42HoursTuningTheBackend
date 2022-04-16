b62763147ef2fd32d4406835b6966f1663567382
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-04-16 20:21:55 +0900

    fix: index

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -4,6 +4,8 @@ ALTER TABLE record ADD INDEX index_record_id(record_id);
 ALTER TABLE file ADD INDEX index_file_id(file_id);
 ALTER TABLE record_item_file ADD INDEX index_linked_record_id(linked_record_id);
 ALTER TABLE group_member ADD INDEX index_user_id(user_id);
+ALTER TABLE record_last_access ADD INDEX index_user_and_record(user_id, record_id);
 
 -- explain select count(*) from group_member;
 
+-- select * from record_comment where linked_record_id = 1 order by created_at desc;
