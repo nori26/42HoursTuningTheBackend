e3a679fed13a5d73553ca9f686d73e141ef03538
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-04-16 18:11:59 +0900

    add: index

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,2 +1,3 @@
 ALTER TABLE record_comment ADD INDEX index_linked_record_id(linked_record_id);
 ALTER TABLE record ADD INDEX index_record_id(record_id);
+ALTER TABLE file ADD INDEX index_file_id(file_id);
